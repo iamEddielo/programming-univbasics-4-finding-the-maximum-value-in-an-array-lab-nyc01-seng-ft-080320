4de97b633e2ac06ac00c7041e4ddbc72c1a8f4e2
Eddie Lopez <iameddielopez@gmail.com>
2020-07-16 20:04:47 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,7 +2,8 @@ def find_max_value(array)
   # Add your solution here
   count = 0
   max_integer = 0
-  while count< array.length do #same loop for going over element in array
+  #same loop for going over element in array
+  while count< array.length do 
     if max_integer < array[count]
        max_integer = array[count]
     end
